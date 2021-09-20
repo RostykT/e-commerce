f1fa15b74cf4871ab1f75ab5f2f751f6e6948676
Rostyk <rostishka1@gmail.com>
2021-09-20 21:58:10 +0300

    updating app with shop page, collection preview component, collection item component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop-page/shop-page.component";
 import { Route, Switch } from "react-router-dom";
-
-const HatsPage = () => (
-	<div>
-		<h1>Hats Page</h1>
-	</div>
-);
 
 function App() {
 	return (
 		<div>
 			<Switch>
 				<Route exact path="/" component={HomePage} />
-				<Route exact path="/hats" component={HatsPage} />
+				<Route exact path="/shop" component={ShopPage} />
 			</Switch>
 		</div>
 	);
---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,22 @@
+import "./collection-item.style.css";
+import React from "react";
+
+const CollectioItem = ({ id, name, price, imageUrl }) => {
+	return (
+		<div className="collection-item">
+			<div
+				className="image"
+				style={{
+					backgroundImage: `url(${imageUrl})`,
+				}}
+			/>
+
+			<div className="collection-footer">
+				<span className="name">{name}</span>
+				<span className="price">{price}</span>
+			</div>
+		</div>
+	);
+};
+
+export default CollectioItem;
