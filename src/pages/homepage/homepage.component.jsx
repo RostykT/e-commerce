ee93a3aecc7d176cf949329ddd2a96a7967dfc5d
Rostyk <rostishka1@gmail.com>
2021-09-20 18:56:55 +0300

    broken homepage into small component, finish styling home component

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./homepage.style.css";
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => {
+	return <Directory />;
+};
+
+export default HomePage;
