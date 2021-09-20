3e7aba09916fc84c445e99e4a06702449db0112e
Rostyk <rostishka1@gmail.com>
2021-09-21 00:22:57 +0300

    add sign in component, style it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop-page/shop-page.component";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/header/header.component";
+import Register from "./pages/registration-page/registration.component";
 
 function App() {
 	return (
@@ -11,6 +12,7 @@ function App() {
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/shop" component={ShopPage} />
+				<Route exact path="/register" component={Register} />
 			</Switch>
 		</div>
 	);
---
+++ src/pages/registration-page/registration.component.jsx
@@ -0,0 +1,13 @@
+import "./registration.style.css";
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const Register = () => {
+	return (
+		<div className="register">
+			<SignIn />
+		</div>
+	);
+};
+
+export default Register;
