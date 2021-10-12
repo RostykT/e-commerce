929e675dc573ca88cf3b60c7dd28d93a1db1d558
Rostyk <rostishka1@gmail.com>
2021-10-12 23:06:33 +0300

    implement redux persist, localeStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-
+import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
 const MiddleWares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...MiddleWares));
+export const store = createStore(rootReducer, applyMiddleware(...MiddleWares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
